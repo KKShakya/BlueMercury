9be598207db026c5ed8925f1839016a9b1573ae5
pravbablu <work.praveen097@gmail.com>
2022-08-26 19:59:43 +0530

    added footer in module

---
+++ components/modules/footer.js
@@ -0,0 +1,82 @@
+function footer(){
+    
+    return `
+    
+  <!-- main box -->
+
+  <div id="container">
+   
+    <!-- box-1 containing logos and email references -->
+  
+  <div >
+    <h3>Get Expert Beauty Insight & Offers</h3>
+    <input type="text" placeholder="ENTER EMAIL">
+    <p>By continuing, you agree to Bluemercury's <a href="term.html"><u> Privacy Practices.</u></a> </p>
+    <a href="https://twitter.com/i/flow/login"> <img src="https://cdn.pixabay.com/photo/2017/03/24/07/28/twitter-2170426__340.png"> </a> 
+    <a href="https://www.facebook.com/"> <img src="https://cdn.icon-icons.com/icons2/1826/PNG/512/4202110facebooklogosocialsocialmedia-115707_115594.png"> </a> 
+    <a href="https://in.pinterest.com/login/"> <img src="https://cdn-icons-png.flaticon.com/512/145/145808.png"> </a> 
+    <a href="https://www.youtube.com/"> <img src="https://i.pinimg.com/564x/7d/c9/93/7dc993c70d4adba215b87cafdc59d82d.jpg"> </a> 
+    <a href="https://www.instagram.com/accounts/login/"> <img src="https://i.pinimg.com/564x/93/87/90/938790b17acb5b3b8236d65ce8c4fc45.jpg" > </a>  
+    <h6 style="font-size: 11px ;">Copyright 2022 bluemercury. All Rights Reserved.</h6>
+    
+  </div>
+  
+  <!-- box-2 containing BLUEMERCURY -->
+  
+  <div >
+  
+    <h3>BLUEMERCURY</h3>
+     <a href="company.html"> <p>Our Company</p> </a>
+     <a href=""> <p>Store Locator</p> </a> 
+     <a href=""> <p>In-store & Online Events</p> </a> 
+     <a href=""> <p>Spa Menu</p> </a>
+     <a href=""> <p>Carrers</p> </a>   
+     <a href="vendor.html"> <p>Vendors Submission</p> </a>
+     <a href=""> <p>Diversity & Inclusions</p> </a>
+  
+  </div>
+  
+  <!-- box-3 containing MY ACCOUNT -->
+  
+  <div >
+    <h3>MY ACCOUNT</h3> 
+   <a href=""> <p>My Account</p> </a> 
+   <a href=""> <p>BlueRewards</p> </a>
+   <a href=""> <p>Gift Card Balance Checker</p> </a> 
+    
+  
+  </div>
+  
+  <div >
+  
+    <!-- box-4 containing CUSTOMER SERVICE -->
+  
+    <h3>CUSTOMER SERVICE</h3>
+    <a href="contact.html"> <p>Contact Us </p> </a>
+    <a href="help.html"> <p>Help & FAQ</p> </a>
+    <a href=""> <p>Shipping, Return & Exchanges</p> </a>
+    <a href=""> <p>Gift Cards</p> </a>
+    <a href=""> <p>Offer & Promotions</p> </a>
+    <a href="klarna.html"> <p>Klarna</p> </a>
+  
+  
+  </div>
+  
+  <!-- box-5 containing RESOURCES -->
+  
+  <div >
+    <h3>RESOURCES</h3>
+    <a href="term.html"> <p>Terms & Conditions</p> </a>
+    <a href=""> <p> Privacy Notice </p> </a>
+    <a href=""> <p>Do Not Sell My Personal Info</p> </a>
+    <a href=""> <p>CA Privacy Rights</p> </a>
+    <a href=""> <p>Accessibility</p> </a>
+    <a href=""> <p>Affiliate Program</p> </a>
+  
+  </div> 
+  
+  </div>
+    `
+}
+
+export { footer } 
