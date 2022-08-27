ea17502714ef656c1c13c25160e5edbdc3c5305a
Ravi <rssharmakapro@gmail.com>
2022-08-27 16:58:08 -0700

    All updates added by Ravi

--- scripts/New_arrivel.js
+++ scripts/New_arrivel.js
@@ -2,57 +2,121 @@
 let page;
 
 let url;
-function bath(){
-
-    page=2;
-    // console.log(page)
-    // url=`https://sephora.p.rapidapi.com/products/list?categoryId=cat150006&pageSize=20&currentPage=2`
-
-    // let res=await fetch(url,{
-    //     method:'GET',
-    //     headers: {
-    //         'Content-Type':'application/json',
-    //         'X-RapidAPI-Key': '092a07a068msh595681c162b3a40p1f553bjsn2c590ce28b45',
-    //          'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
-    //      }
-    // });
-    // let data=await res.json()
-    // console.log(data)
-    // let datas=data.products
-    // appendData(datas)   
-
+let bath=async()=>{
+    url=`https://sephora.p.rapidapi.com/products/list?categoryId=cat150006&pageSize=20&currentPage=2`
+    let res=await fetch(url,{
+        method:'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'X-RapidAPI-Key': '2d3a7e5e94msh2293d946fc77747p1a6907jsn481aaf792503',
+             'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
+         }
+    });
+    let data=await res.json()
+    let datas=data.products
+    document.querySelector(".box1>p>span").innerText="BATH & BODY"
+    document.querySelector("#box2-heading").innerText="BATH & BODY ITEMS"
+    document.querySelector("#box2-heading2").innerText="These items are specified as Body lotion and Bath items."
+    appendData(datas)   
 }
 
-// console.log(url)
-console.log(page)
+// Category 2
+let Fragrance=async()=>{
+    url=`https://sephora.p.rapidapi.com/products/list?categoryId=cat150006&pageSize=20&currentPage=3`
+    let res=await fetch(url,{
+        method:'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'X-RapidAPI-Key': '2d3a7e5e94msh2293d946fc77747p1a6907jsn481aaf792503',
+             'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
+         }
+    });
+    let data=await res.json()
+    let datas=data.products
+    document.querySelector(".box1>p>span").innerText="FRAGRANCE"
+    document.querySelector("#box2-heading").innerText="FRAGRANCE"
+    document.querySelector("#box2-heading2").innerText="These items are specially for fragrance like perfumes and Fragrance Creams."
+    appendData(datas)   
+}
 
+// Category 3
+let Hair=async()=>{
+    url=`https://sephora.p.rapidapi.com/products/list?categoryId=cat150006&pageSize=20&currentPage=4`
+    let res=await fetch(url,{
+        method:'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'X-RapidAPI-Key': '2d3a7e5e94msh2293d946fc77747p1a6907jsn481aaf792503',
+             'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
+         }
+    });
+    let data=await res.json()
+    let datas=data.products
+    document.querySelector(".box1>p>span").innerText="HAIR CARE"
+    document.querySelector("#box2-heading").innerText="HAIR CARE"
+    document.querySelector("#box2-heading2").innerText="These items are specially designed for hairs like straightner and waxes."
+    appendData(datas)   
+}
+//category 4
+let Skins=async()=>{
+    url=`https://sephora.p.rapidapi.com/products/list?categoryId=cat150006&pageSize=20&currentPage=5`
+    let res=await fetch(url,{
+        method:'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'X-RapidAPI-Key': '2d3a7e5e94msh2293d946fc77747p1a6907jsn481aaf792503',
+             'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
+         }
+    });
+    document.querySelector(".box1>p>span").innerText="SKIN"
+    document.querySelector("#box2-heading").innerText="SKIN"
+    document.querySelector("#box2-heading2").innerText="These items are specially designed for skin care. Here items speicfied for all types of skin."
+    let data=await res.json()
+    let datas=data.products
+    appendData(datas)   
+}
+
+//Category 5
+let Makeup=async()=>{
+    url=`https://sephora.p.rapidapi.com/products/list?categoryId=cat150006&pageSize=20&currentPage=6`
+    let res=await fetch(url,{
+        method:'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'X-RapidAPI-Key': '2d3a7e5e94msh2293d946fc77747p1a6907jsn481aaf792503',
+             'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
+         }
+    });
+    document.querySelector(".box1>p>span").innerText="MAKEUP ITEMS"
+    document.querySelector("#box2-heading").innerText="MAKEUP ITEMS"
+    document.querySelector("#box2-heading2").innerText="These items are specially designed for instant makeup and full bride makeup also."
+    let data=await res.json()
+    let datas=data.products
+    appendData(datas)   
+}
 
 
 
 let main=async()=>{
-
-
-
-
+    document.querySelector(".box1>p>span").innerText="NEW ARRIVELS"
+    document.querySelector("#box2-heading").innerText="NEW ARRIVELS"
+    document.querySelector("#box2-heading2").innerText="These items are newly introduced items of our company."
     url=`https://sephora.p.rapidapi.com/products/list?categoryId=cat150006&pageSize=20&currentPage=${page}`
     
 let res=await fetch(url,{
     method:'GET',
     headers: {
         'Content-Type':'application/json',
-		'X-RapidAPI-Key': '092a07a068msh595681c162b3a40p1f553bjsn2c590ce28b45',
+		'X-RapidAPI-Key': '2d3a7e5e94msh2293d946fc77747p1a6907jsn481aaf792503',
  		'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
  	}
 });
-
 let data=await res.json()
-console.log(data)
 let datas=data.products
 appendData(datas)
-console.log(datas)
+
 
 document.querySelector("#sort").addEventListener("change",handleNameSort)
-
 function handleNameSort(){
     let selected=document.querySelector("#sort").value
     
@@ -120,9 +184,41 @@ function appendData(data){
         document.querySelector(".result>span").innerText=count;
     })
 }
-
 function item(elem){
     console.log(elem)
     localStorage.setItem('items',JSON.stringify(elem))
     window.location.href="items.html"
+}
+
+// Best items lists
+
+
+let bitem1=async()=>{
+    url=`https://sephora.p.rapidapi.com/products/detail?productId=P500307&preferedSku=2210607`
+    let res=await fetch(url,{
+        method:'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'X-RapidAPI-Key': '2d3a7e5e94msh2293d946fc77747p1a6907jsn481aaf792503',
+             'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
+         }
+    });
+    let datav=await res.json()
+    localStorage.setItem("bestItem",JSON.stringify(datav))  
+    window.location.href="./best_item.html"
+}
+
+let blip=async()=>{
+    url=`https://sephora.p.rapidapi.com/products/detail?productId=P236816&preferedSku=2210607`
+    let res=await fetch(url,{
+        method:'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'X-RapidAPI-Key': '2d3a7e5e94msh2293d946fc77747p1a6907jsn481aaf792503',
+             'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
+         }
+    });
+    let datav=await res.json()
+    localStorage.setItem("bestItem",JSON.stringify(datav))  
+    window.location.href="./best_item.html"
 }
